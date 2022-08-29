1d3af3aba66b830c91077fcb3b0d363ce9ec4daa
NBKBARIS <69206317+NBKBARIS@users.noreply.github.com>
2022-08-29 20:06:01 +0300

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -1,20 +1,15 @@
 export default function Tos() {
   return (
     <div>
-     <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
-      <p className="text-sm text-white/30 mb-1">Last Update: 20/05/2022 🆕️</p>
+     <h2 className="mt-4 text-white font-bold text-3xl">Hizmet şartları</h2>
+      <p className="text-sm text-white/30 mb-1">Son Güncelleme: 29/08/2022 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-<p>⊳ Every discord server can use the Astra bot for free, it is completely prohibited to sell or market for a fee.</p>
+<p>⊳Her discord sunucusu PikOto botunu ücretsiz olarak kullanabilir, ücretli satış ve pazarlaması kesinlikle yasaktır..</p>
 <div className="py-3"></div>
-<p>⊳ You cannot use bot commands to harm the bot.</p>
 <div className="py-3"></div>
-<p>⊳ You may not use open source codes to harm other servers.</p>
 <div className="py-3"></div>
-<p>⊳ You may not sell or market open source codes through other platforms.</p>
+<p>⊳ Botu kullanırken diğer kullanıcılara zarar verecek şekilde trol amaçlı kullanamazsınız..</p>
 <div className="py-3"></div>
-<p>⊳ While using the bot, you cannot use it in a way that harms other users for troll purposes.</p>
-<div className="py-3"></div>
-<p>⊳ To use certain discord bot commands, you need to vote for the <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">Astra</a> bot at certain intervals.</p>
 </div>
     </div>
   );
