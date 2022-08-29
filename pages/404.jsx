e162a220fb1bd3c24575073b6db0b3927641373f
NBKBARIS <69206317+NBKBARIS@users.noreply.github.com>
2022-08-29 20:24:01 +0300

    Update 404.jsx

--- pages/404.jsx
+++ pages/404.jsx
@@ -3,7 +3,7 @@ const UnkownPage = () => {
     let messages = [
         'Yolunu mu kaybettin evlat?',
         'Lanet olsun! Bu çıkmaz yol.',
-        'Seherbazlar bu yolu kapattı, Harry!'
+        'Sihirbazlar bu yolu kapattı, Harry!'
     ];
 
     return <ErrorPage code={404} message={messages[Math.floor(Math.random()*messages.length)] || "Page not found."} />
