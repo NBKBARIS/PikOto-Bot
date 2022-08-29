e0a9264c4c6822a806747390e45112f6b787fe52
NBKBARIS <69206317+NBKBARIS@users.noreply.github.com>
2022-08-29 20:20:25 +0300

    Update Header.jsx

--- components/Static/Header.jsx
+++ components/Static/Header.jsx
@@ -76,7 +76,7 @@ const Header = ({ $, NavItems }) => {
                 <div className="max-w-7xl px-5 mx-auto py-5 flex items-center justify-between">
                     <div className="flex items-center space-x-6">
                         <div className="flex items-center space-x-3">
-                            <img src="/img/logo2.png" 
+                            <img src="/img/PikOto.png" 
                                 className={`
                                     rounded-full 
                                 `}
@@ -166,7 +166,7 @@ const Header = ({ $, NavItems }) => {
                                 </Menu.Items>
                             </Transition>
                         </Menu>
-                        <Link href="https://bit.ly/3PHDjyC">
+                        <Link href="https://discord.com/oauth2/authorize?client_id=1013124810936684655&scope=bot%20applications.commands&permissions=2146958847">
                             <a className="w-auto flex items-center justify-center shadow-lg gap-x-2 shadow-amber-600/20 rounded-xl py-2.5 font-medium px-7 bg-gradient-to-tl from-amber-500 to-amber-700 text-white  hover:opacity-80 transition duration-200">
                                Invite
                             </a>
